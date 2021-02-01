341c0e022aed97aa28cb3e3e33fb26b5f2389dd7
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-02-02 00:09:25 +0600

    ok

---
+++ map-filter.js
@@ -0,0 +1,33 @@
+const numbers = [3,2,4,2,4,2,4,6,8];
+// const output = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// console.log(output);
+
+// function square(element){
+//     return element*element;
+// }
+// numbers.map(square());
+
+// const result = numbers.map(function(element, index, array){
+//     // console.log(element, index, array);     //map er bitor function korle 3 ta parameter check kra ja
+//     return element*element;
+// })
+// console.log(result);
+
+// const square = element => element*element;
+// const square = x => x*x;
+
+// const result = numbers.map(x => x*x);
+// console.log(result);
+
+const bigger = numbers.filter(x => x>5);
+const bigger1 = numbers.find(x => x>5);     //i1st er 1ta vaule dibe
+console.log(bigger1);
+
+//map and fillter array return kre
