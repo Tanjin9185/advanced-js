960ad1ad2147ce0e2060b6da21ba0c7e54423cf1
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-02-01 23:29:54 +0600

    equal

---
+++ double-vs-triple.js
@@ -0,0 +1,11 @@
+const first = 0;
+const second = 1;
+
+if(first == second){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+// == value check kre.., 0=false, 1=true, 2="2"
+// === value and type both check kre
