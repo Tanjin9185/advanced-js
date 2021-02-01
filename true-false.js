ecf3c2161c100ec215ff102e44361cb2a0c62bd8
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-02-01 22:46:30 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+const age = 4;                          //0 sara  j kono value dile condition true hbe
+const name = "Tanjin"                   //empty strung false
+const id;
+console.log(id);                //variable a value na rakhle undefined..and all undefined false
+if(name){
+    console.log("Condition is True");   
+}
+else{
+    console.log("Condition is False");
+}
+
+//0, "", undefined, null, NaN, fasle,    egula false
+//"0", " ",[],{},"false",true            egula true
